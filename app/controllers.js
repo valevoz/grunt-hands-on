7c556f50b745920f50811086645ec7771ae7df15
Valentine.V5k <Valentine.V5k@gmail.com>
2015-02-20 00:04:03 -0500

    adding dynamics

--- app/controllers.js
+++ app/controllers.js
@@ -1,9 +1,9 @@
-
-
 app.controller('GruntController', function ($scope, $filter, $http) {
 
     $http.get('../app/data.json').success(function (data) {
-        $scope.colors = data.colors;
+        $scope.colors = data.colors.map(function (color) {
+            return {top: Math.floor(Math.random() * 100), left: Math.floor(Math.random() * 100), size: (Math.floor(Math.random() * 30) + 10), color: color}
+        });
     }).error(function (error, status) {
         alert("Source JSON file not found.");
         $scope.colors = [];
